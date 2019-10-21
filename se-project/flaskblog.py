17d6af18e7fe04b82531495bf7939eff00801f51
vidhi-kumar <55309127+vidhi-kumar@users.noreply.github.com>
2019-10-21 23:27:24 +0530

    Add files via upload

---
+++ se-project/flaskblog.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+posts = [
+    {
+        'type' : 'WatchMan',
+        'units' : 10,
+        'date' : '10/10/19'
+    },
+    {
+        'type' : 'BodyGuard',
+        'units' : 2,
+        'date' : '16/12/19'
+    }
+]
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+if __name__ == '__main__':
+    app.run(debug=True)
